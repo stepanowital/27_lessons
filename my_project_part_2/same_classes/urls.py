8c42d1f419c44efff6c073b9207c732551c160ac
lisvv <81390205+lisvv@users.noreply.github.com>
2022-01-27 18:27:48 +0300

    fix urls

--- my_project_part_2/same_classes/urls.py
+++ my_project_part_2/same_classes/urls.py
@@ -1,11 +1,8 @@
 from same_classes import views
 from django.urls import path
 
+# TODO здесь необходимо настроить urls
 urlpatterns = [
    path("feedback/", views.FeedbackView.as_view(), name="feedback"),
    path("feedback/<int:pk>/", views.FeedbackEntityView.as_view(), name="feedback_entity"),
-   path("destination/", views.DestinationView.as_view()),
-   path("destination/<int:pk>/", views.DestinationEntityView.as_view()),
-   path("gen-destination/", views.DestinationListView.as_view()),
-   path("gen-destination/<int:pk>/", views.DestinationDetailView.as_view()),
 ]
