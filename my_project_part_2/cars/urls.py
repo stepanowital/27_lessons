19fad93493a70192dcad4ee1dde0b3e029720c0c
lisvv <81390205+lisvv@users.noreply.github.com>
2022-02-11 23:57:30 +0300

    cars fix (#2)

--- my_project_part_2/cars/urls.py
+++ my_project_part_2/cars/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 # TODO здесь необходимо настроить urls
 urlpatterns = [
-    path("<int:pk>/", views.car, name="car"),
+    path("<int:pk>/", views.cars, name="car"),
 ]
